e00b27ef1b762e075d564daba8d26f87affe88a2
Tanmay Kolase <tanmay.kolase@p99soft.com>
2026-08-24 12:05:14 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.cs
@@ -0,0 +1,13 @@
+//HashSet
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        HashSet<int> seen = new HashSet<int>();
+        foreach (int num in nums) {
+            if (seen.Contains(num)) {
+                return true;
+            }
+            seen.Add(num);
+        }
+        return false;
+    }
+}
