b68db6b8f1cc2a2568dfcc58c998e4f553c67b37
Tanmay Kolase <tanmay.kolase@p99soft.com>
2026-08-29 18:25:59 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cs
@@ -0,0 +1,30 @@
+// Encoding & Decoding
+public class Solution {
+
+    public string Encode(IList<string> strs) {
+        StringBuilder str = new StringBuilder();
+        foreach(string s in strs)
+        {
+            str.Append(s.Length).Append('#').Append(s);
+        }
+        return str.ToString();
+
+    }
+
+    public List<string> Decode(string s) {
+        List<string> res = new List<string>();
+        int i = 0; 
+        while(i < s.Length)
+        {
+            int j = i;
+            while (s[j] != '#')
+                j++;
+            int length = int.Parse(s.Substring(i, j-i));
+            i = j + 1;
+            j = i + length; 
+            res.Add(s.Substring(i, length));
+            i = j;
+        }
+        return res;
+   }
+}
